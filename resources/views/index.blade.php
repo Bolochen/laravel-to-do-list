48395753833c02bca23dcd7b3730218362001d02
Bolo Chen <bolosstuff12@gmail.com>
2023-03-02 14:40:48 +0700

    membuat kerangka layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('container')
+
+    <div class="container text-center mx-auto">
+        <div class="row">
+            <div class="col-sm-4 border">
+                <h1>foto</h1>
+                <h2>text</h2>
+            </div>
+            <div class="col-sm-8 border">
+                <input type="text">
+                <h2>Your undone tasks</h2>
+                <h3>Tidur</h3>
+                <h3>Mandi</h3>
+            </div>
+        </div>
+</div>
+  
+    
+@endsection
